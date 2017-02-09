e4ef65435b6e8e76050a974726a3a432b87cd8ba
Mitja Kramberger <mitja.kramberger@gmail.com>
2017-02-09 23:20:42 +0000

    Initial import

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const OrderBoard = require('./lib/board');
+const OrderTypes = require('./lib/types');
+
+module.exports = {
+  OrderBoard,
+  OrderTypes
+};
---
+++ test/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const { OrderBoard, OrderTypes } = require('../index');
+
+const orderIdA = OrderBoard.register({
+  userId: 42, quantity: 3.5, price: 306, type: OrderTypes.sell
+});
+
+const orderIdB = OrderBoard.register({
+  userId: 40, quantity: 1.2, price: 310, type: OrderTypes.sell
+});
+
+const orderIdC = OrderBoard.register({
+  userId: 40, quantity: 1.5, price: 307, type: OrderTypes.sell
+});
+
+const orderIdD = OrderBoard.register({
+  userId: 40, quantity: 2.0, price: 306, type: OrderTypes.sell
+});
+
+const orderIdE = OrderBoard.register({
+  userId: 40, quantity: 2.0, price: 306, type: OrderTypes.buy
+});
+
+const orderIdI = OrderBoard.register({
+  userId: 40, quantity: 2.0, price: 308, type: OrderTypes.buy
+});
+
+OrderBoard.cancel(orderIdI);
+
+OrderBoard.debug();
